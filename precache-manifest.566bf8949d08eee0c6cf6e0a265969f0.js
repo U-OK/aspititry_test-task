d2ac8716e580fe95e92140444135dd995ba5ff0d
U-OK <thelavallet@gmail.com>
2020-10-22 21:47:15 +0700

    Updates

---
+++ precache-manifest.566bf8949d08eee0c6cf6e0a265969f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20ca208d16f7bef40d3505b69213e0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4474809bfa6200fde78",
+    "url": "/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "71dd10292e01dfd7c1d3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4474809bfa6200fde78",
+    "url": "/static/js/2.34712970.chunk.js"
+  },
+  {
+    "revision": "237da00242ca24e9637cd1222d3370cf",
+    "url": "/static/js/2.34712970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71dd10292e01dfd7c1d3",
+    "url": "/static/js/main.0c8845f7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
