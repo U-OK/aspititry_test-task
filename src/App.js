9add352eb311378d6dece48030fadbcb14aeb6ba
U-OK <thelavallet@gmail.com>
2020-10-23 15:47:07 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Col, Row, Divider } from "antd";
 
 import TrainingTable from "./components/trainingTable/trainingTable";
 import Chart from "./components/chart/chart";
 
+import { getWorkoutData } from "./redux/actionCreator";
+
 function App() {
-  const data = useSelector(state => state.workouts)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWorkoutData());
+  }, []);
+
+  const data = useSelector((state) => state.workouts);
 
   return (
     <>
       <Divider orientation="left">Distance chart</Divider>
       <Row>
         <Col span={14} offset={5}>
-          <Chart data={data}/>
+          <Chart data={data} />
         </Col>
       </Row>
       <Divider orientation="left">Workout table</Divider>
       <Row>
         <Col span={18} offset={3}>
-          <TrainingTable data={data}/>
+          <TrainingTable data={data} />
         </Col>
       </Row>
     </>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,61 +1,17 @@
 import {
-  GET_WORKOUT_DATA,
-  DELETE_WORKOUT_BY_ID,
-  ADD_NEW_WORKOUT,
-  EDIT_WORKOUTS
+  GET_WORKOUT_DATA_SUCCESS
 } from "./actionTypes";
 
 const initialState = {
-  workouts: [
-    {
-      id: 0,
-      workoutType: "bicycling",
-      distance: 3,
-      date: "2020-10-20",
-      comment: "",
-    },
-    {
-      id: 1,
-      workoutType: "running",
-      distance: 15,
-      date: "2020-10-21",
-      comment: "cool",
-    },
-    {
-      id: 2,
-      workoutType: "walking",
-      distance: 14,
-      date: "2020-10-22",
-      comment: "",
-    },
-  ],
+  workouts: [],
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case GET_WORKOUT_DATA: {
+    case GET_WORKOUT_DATA_SUCCESS: {
       return {
         ...state,
         workouts: [...action.payload],
-      };
-    }
-    case ADD_NEW_WORKOUT: {
-      return {
-        ...state,
-        workouts: [...state.workouts, action.payload],
-      };
-    }
-    case EDIT_WORKOUTS: {
-      return {
-        workouts: [...action.payload]
-      };
-    }
-    case DELETE_WORKOUT_BY_ID: {
-      return {
-        ...state,
-        workouts: [
-          ...state.workouts.filter((item) => item.id !== action.payload),
-        ],
       };
     }
     default:
